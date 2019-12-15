9b2bcf1a5914e2ffce05d2182fccb3d491b1224c
Rupinder <rupinder.developer@gmail.com>
2019-12-15 14:11:10 +0530

    Implementation static varibales in Store

--- src/index.js
+++ src/index.js
@@ -6,8 +6,13 @@ import Model from './lib/Model';
 
 class MySQLees {
     connect(mysql, config) {
+        Store.created_models = {};
+        Store.pending_fk_queries = []; // Pending Foreign Keys Queries
         Store.config = config;
         Store.connection = mysql.createConnection({...config, multipleStatements: true});
+        Store.connection.connect(function(err) {
+            if (err) console.log(err);
+        }); 
         return Store.connection;
     }
 
--- src/lib/Store.js
+++ src/lib/Store.js
@@ -1,13 +1,3 @@
 "use strict"
 
-class Store {
-    constructor() {
-        this.connection = null;
-        this.config = null;
-
-        this.created_models = {};
-        this.pending_fk_queries = []; // Pending Foreign Keys Queries
-    }
-}
-
-module.exports = new Store();
+module.exports = class Store {}
