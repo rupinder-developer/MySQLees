700a4521bef82eb73ccc7e2f60f2b9a490d47939
Rupinder <rupinder.developer@gmail.com>
2020-01-18 23:11:17 +0530

    Added new sample file

--- index-2.js
+++ index-2.js
@@ -42,7 +42,8 @@ const members = mysqlees.schema({
     },
     password: {
         datatype: {
-            name: 'longtext'
+            name: 'varchar',
+            size: 255
         }
     },
     type: {
@@ -60,5 +61,111 @@ const members = mysqlees.schema({
     timestamps: true
 });
 
-
 mysqlees.model('members', members);
+
+
+const products = mysqlees.schema({
+    id: {
+        autoIncrement: true,
+        primaryKey: true,
+        datatype: {
+            name: 'int'
+        }
+    },
+    brand: {
+        datatype: {
+            name: 'varchar',
+            size: 255
+        }
+    },
+    purchase_invoice_number: {
+        datatype: {
+            name: 'varchar',
+            size: 255
+        }
+    },
+    purchase_invoice_date: {
+        datatype: {
+            name: 'varchar',
+            size: 255
+        }
+    },
+    product_name: {
+        datatype: {
+            name: 'varchar',
+            size: 255
+        }
+    },
+    eancode: {
+        datatype: {
+            name: 'varchar',
+            size: 255
+        },
+        ref: {
+            to: 'inventory',
+            foreignField: 'eancode'
+        },
+    },
+    mrp: {
+        datatype: {
+            name: 'int'
+        }
+    },
+    customer_discount: {
+        datatype: {
+            name: 'int'
+        }
+    },
+    lp: {
+        datatype: {
+            name: 'int'
+        }
+    },
+    qty: {
+        datatype: {
+            name: 'int'
+        }
+    },
+    category: {
+        datatype: {
+            name: 'varchar',
+            size: 255
+        }
+    },
+    qty: {
+        datatype: {
+            name: 'int'
+        }
+    },
+    cost_price: {
+         datatype: {
+            name: 'int'
+        }
+    },
+    filename: {
+        datatype: {
+            name: 'varchar',
+            size: 255
+        }
+    }
+}, {timestamps: true});
+
+
+mysqlees.model('products', products);
+
+const inventory = mysqlees.schema({
+    eancode: {
+        datatype: {
+            name: 'varchar',
+            size: 255
+        },
+        primaryKey: true
+    },
+    qty: {
+        datatype: {
+            name: 'int'
+        }
+    }
+}, {timestamps: true});
+
+mysqlees.model('inventory', inventory);
