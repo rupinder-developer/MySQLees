9ab153e493fada61f9f71e4606e76bf111ced130
Rupinder <rupinder.developer@gmail.com>
2020-07-03 17:58:25 +0530

    Improve Error Handling while migration

--- src/lib/Schema.js
+++ src/lib/Schema.js
@@ -633,7 +633,15 @@ module.exports = class Schema {
                 database: Schema.config.database,
                 multipleStatements: true
             });
-            console.log('Migrating....');
+            
+            Schema.connection.connect(function(err) {
+                if (err) {
+                    console.error(err);
+                    process.exit();
+                } else {
+                    console.log('Migrating....');
+                }
+            }); 
         }
     }
 
